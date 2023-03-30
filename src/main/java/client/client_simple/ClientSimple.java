e14e29b4e94acaa19a5f3c04e13d0f36fcd477e1
Germain STANG <germain.stang@gmail.com>
2023-03-30 15:29:52 -0400

    v1 javadoc server

--- src/main/java/client/client_simple/ClientSimple.java
+++ src/main/java/client/client_simple/ClientSimple.java
@@ -1,7 +1,27 @@
 package client.client_simple;
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 
 public class ClientSimple {
     public static void main(String[] args) {
-
+        try {
+            Socket clientSocket = new Socket("127.0.0.1", 1337);
+            OutputStreamWriter os = new OutputStreamWriter(
+                    clientSocket.getOutputStream()
+            );
+            BufferedWriter writer = new BufferedWriter(os);
+            Scanner scanner = new Scanner(System.in);
+            while(scanner.hasNext()) {
+                String line = scanner.nextLine();
+                System.out.println("Envoi de : " + line);
+                writer.append(line + "\n");
+                writer.flush();
+            }
+            writer.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
